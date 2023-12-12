119efd0c20b1f3cb79b35f86c3d331a8de1c0cab
mohammadvaladbiegi2 <126916758+mohammadvaladbiegi2@users.noreply.github.com>
2023-12-12 16:25:50 +0330

    fix rout

--- src/routs.js
+++ src/routs.js
@@ -1,35 +1,16 @@
-import React from "react";
 import About from "./page/About/About";
 import BusinessRelocation from "./page/BusinessRelocation/BusinessRelocation";
 import GeneralDisclaimer from "./page/GeneralDisclaimer/GeneralDisclaimer";
 import Homepage from "./page/Homepage/Homepage";
-const LazyHomepage = React.lazy(() => import("./page/Homepage/Homepage"));
-const LazyAbout = React.lazy(() => import("./page/About/About"));
 import InvestmentOpportunities from "./page/InvestmentOpportunities/InvestmentOpportunities";
 import Services from "./page/Services/Services";
 import Migration from "./page/Migration/Migration";
 import ContactUs from "./componant/ContactUs/ContactUs";
 import FamilyRelocations from "./page/FamilyRelocations/FamilyRelocations";
-import Loader from "./componant/Loader/Loader";
 
 const routs = [
-  {
-    path: "/",
-    element: (
-      <React.Suspense fallback={<Loader />}>
-        <LazyHomepage />
-      </React.Suspense>
-    ),
-  },
-  {
-    path: "/about",
-    element: (
-      <React.Suspense fallback={<Loader />}>
-        <LazyAbout />
-      </React.Suspense>
-    ),
-  },
-  // { path: "/about", element: <About /> },
+  { path: "/", element: <Homepage /> },
+  { path: "/about", element: <About /> },
   { path: "/businessRelocation", element: <BusinessRelocation /> },
   { path: "/generalDisclaimer", element: <GeneralDisclaimer /> },
   { path: "/investmentOpportunities", element: <InvestmentOpportunities /> },
