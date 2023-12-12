b871f8723ce1175960f6997d5829084e1612019c
mohammadvaladbiegi2 <126916758+mohammadvaladbiegi2@users.noreply.github.com>
2023-12-12 16:13:32 +0330

    add Loader

---
+++ src/componant/Loader/Loader.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Loader.css";
+
+export default function Loader() {
+  return (
+    <div id="loading-animation" class="heading large-text">
+      <p>l</p>
+      <p>o</p>
+      <p>a</p>
+      <p>d</p>
+      <p>i</p>
+      <p>n</p>
+      <p>g</p>
+    </div>
+  );
+}
--- src/routs.js
+++ src/routs.js
@@ -1,15 +1,25 @@
+import React from "react";
 import About from "./page/About/About";
 import BusinessRelocation from "./page/BusinessRelocation/BusinessRelocation";
 import GeneralDisclaimer from "./page/GeneralDisclaimer/GeneralDisclaimer";
 import Homepage from "./page/Homepage/Homepage";
+const LazyHomepage = React.lazy(() => import("./page/Homepage/Homepage"));
 import InvestmentOpportunities from "./page/InvestmentOpportunities/InvestmentOpportunities";
 import Services from "./page/Services/Services";
 import Migration from "./page/Migration/Migration";
 import ContactUs from "./componant/ContactUs/ContactUs";
 import FamilyRelocations from "./page/FamilyRelocations/FamilyRelocations";
+import Loader from "./componant/Loader/Loader";
 
 const routs = [
-  { path: "/", element: <Homepage /> },
+  {
+    path: "/",
+    element: (
+      <React.Suspense fallback={<Loader />}>
+        <Homepage />
+      </React.Suspense>
+    ),
+  },
   { path: "/about", element: <About /> },
   { path: "/businessRelocation", element: <BusinessRelocation /> },
   { path: "/generalDisclaimer", element: <GeneralDisclaimer /> },
